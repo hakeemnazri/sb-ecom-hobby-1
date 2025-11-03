80dbfd3b880a378901a32374e48b098a54a0cbd7
hakeemnazri <mhakeemnazri@gmail.com>
2025-11-03 09:45:22 +0800

    init docker

--- src/main/java/com/sb_hobby/ecom/category/entities/Category.java
+++ src/main/java/com/sb_hobby/ecom/category/entities/Category.java
@@ -1,4 +1,19 @@
 package com.sb_hobby.ecom.category.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long categoryId;
 }
