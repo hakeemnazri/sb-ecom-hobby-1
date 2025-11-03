77115c8aa5deadeb7569bab501a2492f716dd711
hakeemnazri <mhakeemnazri@gmail.com>
2025-11-03 10:17:11 +0800

    Add: Interface category service

--- src/main/java/com/sb_hobby/ecom/category/CategoryService.java
+++ src/main/java/com/sb_hobby/ecom/category/CategoryService.java
@@ -1,4 +1,14 @@
 package com.sb_hobby.ecom.category;
 
+import com.sb_hobby.ecom.category.DTO.CategoryDTO;
+import com.sb_hobby.ecom.category.DTO.CategoryResponse;
+
 public interface CategoryService {
+    CategoryResponse getAllCategories (Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+
+    CategoryDTO createCategory (CategoryDTO categoryDTO);
+
+    CategoryDTO deleteCategory (Long categoryId);
+
+    CategoryDTO updateCategory (CategoryDTO categoryDTO, Long categoryId);
 }
