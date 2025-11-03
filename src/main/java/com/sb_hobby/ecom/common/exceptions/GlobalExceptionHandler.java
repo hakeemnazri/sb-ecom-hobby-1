9f1fcb4ce869138f351281a8e2dc0d52ff6c256f
hakeemnazri <mhakeemnazri@gmail.com>
2025-11-03 12:12:34 +0800

    Add: Payload DTO

---
+++ src/main/java/com/sb_hobby/ecom/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.sb_hobby.ecom.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String fieldName = err.getField();
+            String defaultMessage = err.getDefaultMessage();
+            response.put(fieldName, defaultMessage);
+        });
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<>
+}
